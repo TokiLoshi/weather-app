8092bc681c8cc4cf441b0b9ca04d3fabdbfa4b60
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2022-04-21 10:46:29 -0700

    added icon cleaned up code and reordered elements in html

--- src/app.js
+++ src/app.js
@@ -1,23 +1,5 @@
 function formatDate(timestamp) {
   let now = new Date(timestamp);
-  let date = now.getDate();
-
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let month = months[now.getMonth()];
-  let year = now.getFullYear();
   let hour = now.getHours();
   if (hour < 10) {
     hour = `0${hour}`;
@@ -30,17 +12,6 @@ function formatDate(timestamp) {
   if (seconds < 10) {
     seconds = `0${seconds}`;
   }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[now.getDay()];
-  console.log(day);
   return `${hour}:${minutes}`;
 }
 
@@ -74,9 +45,6 @@ function formatDay(timestamp) {
   ];
   let month = months[now.getMonth()];
   let year = now.getFullYear();
-  console.log(day);
-  // let pDay = document.querySelector("#day");
-  // pDay.innerHTML = `${day}`;
   return `${day}, ${date} ${month}, ${year} `;
 }
 
@@ -110,29 +78,12 @@ function showWeather(response) {
   weatherDescriptionElement.innerHTML = `${response.data.weather[0].description}`;
   timeElement.innerHTML = formatDate(response.data.dt * 1000);
   dayElement.innerHTML = formatDay(response.data.dt * 1000);
-  weatherIconElement.innerHTML = `http://openweathermap.org/img/wn/10d@2x.png`;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png `
+  );
+  weatherIconElement.setAttribute("alt", response.data.weather[0].description);
 }
-// document.querySelector("#user-city").innerHTML = `${response.data.name}`;
-// document.querySelector("#celcius-temp").innerHTML = `${Math.round(
-//   response.data.main.temp
-// )}°C`;
-// document.querySelector(
-//   "#humidity"
-// ).innerHTML = `<strong>Humidity:</strong> ${response.data.main.humidity}%`;
-// document.querySelector(
-//   "#wind"
-// ).innerHTML = `<strong>Wind Speed:</strong> ${Math.round(
-//   response.data.wind.speed
-// )} m/s`;
-// document.querySelector(
-//   "#weather-description"
-// ).innerHTML = `${response.data.weather[0].description}`;
-// document.querySelector("#time").innerHTML = formatDate(response.data.dt * 1000);
-//  document.querySelector("#day").innerHTML = formatDay(response.data.dt * 1000);
-//  document.querySelector(
-//    "#weather-element"
-//  ).innerHTML = `http://openweathermap.org/img/wn/10d@2x.png`;
-
 function searchCity(city) {
   let apiKey = "85b2f9ddbf909c56fc814cf91c0ccce6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
